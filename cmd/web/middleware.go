83647cf9dd6e68da21ff92ebc5df4b9875fb7e72
Larroyo <lucianoarroyo198@gmail.com>
2022-10-16 22:10:15 -0500

    Create middleware.go
    Added the csrf protection for post requests

---
+++ cmd/web/middleware.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/justinas/nosurf"
+	"net/http"
+)
+
+// NoSurf adds CSRF protection to all POST requests
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   app.InProduction, // set true when going in production
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
+
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
+}
