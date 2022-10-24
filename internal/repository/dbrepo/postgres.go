b2d1682799b8857fa024b94efc87ecce7b7e1d55
iiyuuu <nnathan72@yahoo.com>
2022-10-24 18:33:57 -0500

    added more queries

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -1,12 +1,39 @@
 package dbrepo
 
-func (n *postgresDBRepo) AddUser(res models.user) err {
+import "github.com/DeLuci/coog-music/internal/models"
+
+func (n *postgresDBRepo) AddUser(res models.Users) err {
 	query := "insert into users (username, password) values ($1, $2)"
 
-	_, err := m.DB.Exec(query, rest.username, res.password)
+	_, err := m.DB.Exec(query, res.Username, res.Password)
 	if err != nil {
 		return err
 	}
 
 	return nil
 }
+
+func (n *postgresDBRepo) AddSong(res models.Song) err{
+	query := "insert into song (title, artist_name) values ($1, $2)"
+
+	_, err := m.Db.Exec(query, res.Artist_name, res.Title)
+	if err != nil{
+		return err
+	}
+
+	return nil
+}
+
+func (n* postgresDBRepo) AddSongToPlaylist(res models.Song, models.Playlist) err{
+	query := "insert into playlist (playlist.songs) values($1)"
+
+	_, err := m.Db. Exec(query, res.song_id)
+	if err != nil{
+		return err
+	}
+
+	return nil
+}
+
+//play song (select and songplay session)
+//add song to album (artist thing
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/DeLuci/coog-music/internal/models"
+
 type DatabaseRepo interface {
-	AddUser(res models.user) err
+	AddSong(res models.Song)
+	AddUser(res models.Users)
+	AddSongToPlaylist(res models.Song, models.Playlist)
 }
